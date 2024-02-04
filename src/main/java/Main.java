2e2880b0ec1bc60c5012fdfe61101bb5f4f3f400
Zerhood <Zerhood111@ya.ru>
2024-02-04 16:58:04 +0700

    create homework2_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,35 @@
 public class Main {
     public static void main(String[] args) {
+        try {
+            checkAccount("awdawd", "awd", "awd");
+        } catch (WrongLoginException | WrongPasswordException w) {
+            System.out.println(w.getMessage());
+        }
+    }
+
+    public static void checkAccount(String login, String password, String confirmPassword) {
+        String regex = "([a-zA-Z\\d+_]{3,20}+)";
+
+        if (!login.matches(regex)) {
+            throw new WrongLoginException("Ошибка ввода логина!");
+        }
+        if (!password.matches(regex)) {
+            throw new WrongPasswordException("Ошибка ввода пароля!");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Ошибка ввода пароля!");
+        }
+    }
+}
+
+class WrongLoginException extends RuntimeException {
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
+
+class WrongPasswordException extends RuntimeException {
+    public WrongPasswordException(String message) {
+        super(message);
     }
 }
